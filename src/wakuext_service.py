a3b93e05c8df581c111ee9ede93a43563bf8404c
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-06-30 23:06:40 +0200

    Add more missing typing

--- src/wakuext_service.py
+++ src/wakuext_service.py
@@ -19,28 +19,28 @@ class WakuextService(Service):
             assert json_response["error"]["message"] == "messenger already started"
             return
 
-    def create_community(self, name, color="#ffffff", membership=3) -> Dict:
+    def create_community(self, name: str, color="#ffffff", membership:int = 3) -> Dict:
         # TODO check what is membership = 3
         params = [{"membership": membership, "name": name, "color": color, "description": name}]
         response = self.rpc_request("createCommunity", params)
         return response.json()
 
-    def fetch_community(self, community_key) -> Dict:
+    def fetch_community(self, community_key: str) -> Dict:
         params = [{"communityKey": community_key, "waitForResponse": True, "tryDatabase": True}]
         response = self.rpc_request("fetchCommunity", params)
         return response.json()
 
-    def request_to_join_community(self, community_id, address="fakeaddress") -> Dict:
+    def request_to_join_community(self, community_id: str, address: str = "fakeaddress") -> Dict:
         params = [{"communityId": community_id, "addressesToReveal": [address], "airdropAddress": address}]
         response = self.rpc_request("requestToJoinCommunity", params)
         return response.json()
 
-    def accept_request_to_join_community(self, request_to_join_id) -> Dict:
+    def accept_request_to_join_community(self, request_to_join_id: str) -> Dict:
         params = [{"id": request_to_join_id}]
         response = self.rpc_request("acceptRequestToJoinCommunity", params)
         return response.json()
 
-    def send_chat_message(self, chat_id, message, content_type=1) -> Dict:
+    def send_chat_message(self, chat_id: str, message: str, content_type:int = 1) -> Dict:
         # TODO content type can always be 1? (plain TEXT), does it need to be community type for communities?
         params = [{"chatId": chat_id, "text": message, "contentType": content_type}]
         response = self.rpc_request("sendChatMessage", params)
