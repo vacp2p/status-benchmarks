952a375a27a1e87c3dbc6a4bc6fe51b744113bce
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-06-30 22:06:30 +0200

    Add missing typing to RpcClient

--- src/rpc_client.py
+++ src/rpc_client.py
@@ -2,6 +2,8 @@
 import json
 import logging
 import requests
+from typing import List
+from requests import Session, Response
 from tenacity import retry, stop_after_delay, wait_fixed
 from json import JSONDecodeError
 
@@ -10,12 +12,12 @@ from json import JSONDecodeError
 
 class RpcClient:
 
-    def __init__(self, rpc_url, client=requests.Session()):
+    def __init__(self, rpc_url: str, client: Session = requests.Session()):
         self.client = client
         self.rpc_url = rpc_url
         self.request_counter = 0
 
-    def _check_decode_and_key_errors_in_response(self, response, key):
+    def _check_decode_and_key_errors_in_response(self, response: Response, key: str) -> str:
         try:
             return response.json()[key]
         except json.JSONDecodeError:
@@ -23,7 +25,7 @@ class RpcClient:
         except KeyError:
             raise AssertionError(f"Key '{key}' not found in the JSON response: {response.content}")
 
-    def verify_is_valid_json_rpc_response(self, response, _id=None, skip_validation=False):
+    def verify_is_valid_json_rpc_response(self, response: Response, _id: str = None, skip_validation: bool = False) -> Response:
         if skip_validation:
             return response
 
@@ -39,13 +41,14 @@ class RpcClient:
                 raise AssertionError(f"no id in response {response.json()}")
         return response
 
-    def verify_is_json_rpc_error(self, response):
+    def verify_is_json_rpc_error(self, response: Response):
         assert response.status_code == 200
         assert response.content
         self._check_decode_and_key_errors_in_response(response, "error")
 
     @retry(stop=stop_after_delay(10), wait=wait_fixed(0.5), reraise=True)
-    def rpc_request(self, method, params=None, request_id=None, url=None, enable_logging=True):
+    def rpc_request(self, method: str, params: List = None, request_id: str = None, url: str = None,
+                    enable_logging: bool = True) -> Response:
         if not request_id:
             request_id = self.request_counter
             self.request_counter += 1
@@ -69,7 +72,8 @@ class RpcClient:
                 logging.debug(f"Got response: {response.content}")
         return response
 
-    def rpc_valid_request(self, method, params=None, _id=None, url=None, skip_validation=False, enable_logging=True):
+    def rpc_valid_request(self, method: str, params: List = None, _id: str = None, url: str = None,
+                          skip_validation: bool = False, enable_logging: bool = True) -> Response:
         response = self.rpc_request(method, params, _id, url, enable_logging=enable_logging)
         self.verify_is_valid_json_rpc_response(response, _id, skip_validation=skip_validation)
         return response
