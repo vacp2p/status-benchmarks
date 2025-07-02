64f3d90f222a0674314eea2f752bc5e8168f3273
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-07-02 16:36:16 +0200

    Change WakuextService to async due to Service changes

--- src/wakuext_service.py
+++ src/wakuext_service.py
@@ -2,51 +2,51 @@
 from typing import Dict
 
 # Project Imports
-from src.rpc_client import RpcClient
-from src.service import Service
+from src.rpc_client import AsyncRpcClient
+from src.service import AsyncService
 
 
-class WakuextService(Service):
-    def __init__(self, client: RpcClient):
-        super().__init__(client, "wakuext")
+class WakuextAsyncService(AsyncService):
+    def __init__(self, async_rpc_client: AsyncRpcClient):
+        super().__init__(async_rpc_client, "wakuext")
 
-    def start_messenger(self):
-        response = self.rpc_request("startMessenger")
-        json_response = response.json()
+    async def start_messenger(self):
+        response = await self.rpc_request("startMessenger")
+        json_response = await response.json()
 
         if "error" in json_response:
             assert json_response["error"]["code"] == -32000
             assert json_response["error"]["message"] == "messenger already started"
             return
 
-    def create_community(self, name: str, color="#ffffff", membership:int = 3) -> Dict:
+    async def create_community(self, name: str, color="#ffffff", membership: int = 3) -> Dict:
         # TODO check what is membership = 3
         params = [{"membership": membership, "name": name, "color": color, "description": name}]
-        response = self.rpc_request("createCommunity", params)
-        return response.json()
+        response = await self.rpc_request("createCommunity", params)
+        return await response.json()
 
-    def fetch_community(self, community_key: str) -> Dict:
+    async def fetch_community(self, community_key: str) -> Dict:
         params = [{"communityKey": community_key, "waitForResponse": True, "tryDatabase": True}]
-        response = self.rpc_request("fetchCommunity", params)
-        return response.json()
+        response = await self.rpc_request("fetchCommunity", params)
+        return await response.json()
 
-    def request_to_join_community(self, community_id: str, address: str = "fakeaddress") -> Dict:
+    async def request_to_join_community(self, community_id: str, address: str = "fakeaddress") -> Dict:
         params = [{"communityId": community_id, "addressesToReveal": [address], "airdropAddress": address}]
-        response = self.rpc_request("requestToJoinCommunity", params)
-        return response.json()
+        response = await self.rpc_request("requestToJoinCommunity", params)
+        return await response.json()
 
-    def accept_request_to_join_community(self, request_to_join_id: str) -> Dict:
+    async def accept_request_to_join_community(self, request_to_join_id: str) -> Dict:
         params = [{"id": request_to_join_id}]
-        response = self.rpc_request("acceptRequestToJoinCommunity", params)
-        return response.json()
+        response = await self.rpc_request("acceptRequestToJoinCommunity", params)
+        return await response.json()
 
-    def send_chat_message(self, chat_id: str, message: str, content_type:int = 1) -> Dict:
+    async def send_chat_message(self, chat_id: str, message: str, content_type: int = 1) -> Dict:
         # TODO content type can always be 1? (plain TEXT), does it need to be community type for communities?
         params = [{"chatId": chat_id, "text": message, "contentType": content_type}]
-        response = self.rpc_request("sendChatMessage", params)
-        return response.json()
+        response = await self.rpc_request("sendChatMessage", params)
+        return await response.json()
 
-    def send_contact_request(self, contact_id: str, message: str) -> Dict:
+    async def send_contact_request(self, contact_id: str, message: str) -> Dict:
         params = [{"id": contact_id, "message": message}]
-        response = self.rpc_request("sendContactRequest", params)
-        return response.json()
+        response = await self.rpc_request("sendContactRequest", params)
+        return await response.json()
