58914a074d2438ce862ea38d8ac81bbff98e9910
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-07-03 11:53:48 +0200

    Fix double await because of previous changes in RPCClient

--- src/service.py
+++ src/service.py
@@ -10,6 +10,7 @@ class AsyncService:
         self.rpc = async_rpc_client
         self.name = name
 
-    async def rpc_request(self, method: str, params: Optional[list] = None, enable_logging: bool = True) -> Any:
+    async def rpc_request(self, method: str, params: Optional[list] = None, enable_logging: bool = True) -> dict:
+        # In order to be validated, the response is already awaited, so this already returns the dict data
         full_method_name = f"{self.name}_{method}"
         return await self.rpc.rpc_valid_request(full_method_name, params or [], enable_logging=enable_logging)
