ec4557ebbc8924b79c5b0adfd107b45d1d15b104
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-07-03 20:37:03 +0200

    Fix rebased code

--- src/benchmark_scenarios/communities.py
+++ src/benchmark_scenarios/communities.py
@@ -165,9 +165,11 @@ async def request_to_join_community_mix():
     backend_relay_pods = kube_utils.get_pods("status-backend-relay", "status-go-test")
     relay_nodes = await setup_status.initialize_nodes_application(backend_relay_pods)
 
-    logger.info("Shutting down node connections")
-    await asyncio.gather(*[node.shutdown() for node in relay_nodes.values()])
-    logger.info("Finished store_performance")
+    name = f"test_community_{''.join(random.choices(string.ascii_letters, k=10))}"
+    logger.info(f"Creating community {name}")
+    response = relay_nodes["status-backend-relay-0"].wakuext_service.create_community(name)
+    community_id = response.get("result", {}).get("communities", [{}])[0].get("id")
+    logger.info(f"Community {name} created with ID {community_id}")
 
     owner = relay_nodes["status-backend-relay-0"]
     nodes = [key for key in relay_nodes.keys() if key != "status-backend-relay-0"]
