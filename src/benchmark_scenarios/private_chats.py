e2fbb898fcf2523def74ad3d955085d32c7a05c3
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-07-06 19:48:16 +0200

    Add idle_relay scenario

---
+++ src/benchmark_scenarios/private_chats.py
@@ -0,0 +1,29 @@
+# Python Imports
+import asyncio
+import logging
+import random
+
+# Project Imports
+import src.logger
+from src import kube_utils
+from src.setup_status import initialize_nodes_application, send_friend_requests, accept_friend_requests
+
+logger = logging.getLogger(__name__)
+
+async def idle_relay():
+    # 1 relay node alice
+    # 100 relay nodes - friends
+    # friends have accepted contact request with alice, who accepted it
+
+    kube_utils.setup_kubernetes_client()
+    backend_relay_pods = kube_utils.get_pods("status-backend-relay", "status-go-test")
+    relay_nodes = await initialize_nodes_application(backend_relay_pods)
+
+    alice = "status-backend-relay-0"
+    friends = [key for key in relay_nodes.keys() if key != alice]
+    requests_made = await send_friend_requests(relay_nodes, [alice], friends)
+    _ = await accept_friend_requests(relay_nodes, requests_made)
+
+    logger.info("Shutting down node connections")
+    await asyncio.gather(*[node.shutdown() for node in relay_nodes.values()])
+    logger.info("Finished idle_relay")
