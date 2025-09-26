93dfebf31c07b09a68166eb6fc8ab923df049103
Alberto Soutullo <alberto.soutullo639@gmail.com>
2025-09-26 19:05:12 +0200

    Idle light (#22)
    * Add idle_light scenario
    
    * Update src/benchmark_scenarios/private_chats.py
    
    Co-authored-by: PearsonWhite <PearsonWhite602@gmail.com>
    
    ---------
    
    Co-authored-by: PearsonWhite <PearsonWhite602@gmail.com>

--- src/benchmark_scenarios/private_chats.py
+++ src/benchmark_scenarios/private_chats.py
@@ -27,3 +27,22 @@ async def idle_relay():
     logger.info("Shutting down node connections")
     await asyncio.gather(*[node.shutdown() for node in relay_nodes.values()])
     logger.info("Finished idle_relay")
+
+
+async def idle_light():
+    # 1 light node alice
+    # 100 light nodes - friends
+    # friends have accepted contact request with alice
+
+    kube_utils.setup_kubernetes_client()
+    backend_light_pods = kube_utils.get_pods("status-backend-light", "status-go-test")
+    light_nodes = await initialize_nodes_application(backend_light_pods)
+
+    alice = "status-backend-light-0"
+    friends = [key for key in light_nodes.keys() if key != alice]
+    requests_made = await send_friend_requests(light_nodes, [alice], friends)
+    _ = await accept_friend_requests(light_nodes, requests_made)
+
+    logger.info("Shutting down node connections")
+    await asyncio.gather(*[node.shutdown() for node in light_nodes.values()])
+    logger.info("Finished idle_light")
